b759f07cc89e7ad0c974d78a8879d4dec859dc7d
sebug <sebastian.gfeller@gmail.com>
2020-10-07 17:15:55 +0200

    No top level async

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import { getMessage } from './modules/functionCalls.js'
 
-const message = await getMessage();
+(async function () {
+    const message = await getMessage();
 
-const messageParagraph = document.querySelector('p.message');
+    const messageParagraph = document.querySelector('p.message');
 
-messageParagraph.innerHTML = message;
+    messageParagraph.innerHTML = message;
+}());
+
 
 
