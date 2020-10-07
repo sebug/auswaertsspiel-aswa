80bc0346b66daa30cdd9b12e8256edaa1104b6c3
sebug <sebastian.gfeller@gmail.com>
2020-10-07 17:14:52 +0200

    Dealing with async function

--- modules/functionCalls.js
+++ modules/functionCalls.js
@@ -1,5 +1,7 @@
-export async function getMessage() {
+async function getMessage() {
     const messageResult = await fetch('/api/GetMessage?name=SampleUser');
     const resultJson = await messageResult.json();
     return resultJson;
-};
+}
+
+export getMessage;
