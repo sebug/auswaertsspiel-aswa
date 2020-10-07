1d81a65e830d80b0f5f6e030fe70120212d11f7c
sebug <sebastian.gfeller@gmail.com>
2020-10-07 22:04:32 +0200

    Getting environment variable

--- api/GetMessage/__init__.py
+++ api/GetMessage/__init__.py
@@ -1,10 +1,12 @@
 import logging
-
+import os
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    message_text = os.environ["MESSAGE_TEXT"]
+    logging.info(f'The message text is {message_text}')
 
     name = req.params.get('name')
     if not name:
